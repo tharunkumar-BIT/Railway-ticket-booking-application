909219b757339ab58c6a88aecdc6099995428e52
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-06-11 15:37:01 +0530

    Ticket class

---
+++ src/Ticket.java
@@ -0,0 +1,17 @@
+public class Ticket {
+    int id;
+    String passengerName;
+    CoachType coachType;
+    boolean isWaiting;
+
+    public Ticket(int id, String passengerName, CoachType coachType, boolean isWaiting) {
+        this.id = id;
+        this.passengerName = passengerName;
+        this.coachType = coachType;
+        this.isWaiting = isWaiting;
+    }
+
+    public String toString() {
+        return "Ticket ID: " + id + ", Passesger Name: " + passengerName + ", Coach Type: " + coachType + (isWaiting ? " (Waiting) " : " (Confirmed) ");
+    }
+}
